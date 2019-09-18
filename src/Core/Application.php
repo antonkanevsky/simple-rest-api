3f3d95fd70d6c4f4150b9f7d6976faeb250019ea
Anton Kanevskiy <antonkanevsky89@gmail.com>
2019-09-18 09:39:20 +0300

    Routing

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -6,6 +6,10 @@ namespace App\Core;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
 
 /**
  * Базовый класс приложения
@@ -30,8 +34,26 @@ class Application
      *
      * @return Response
      */
-    public function handle(Request $request)
+    public function handle(Request $request): Response
     {
-        return new Response('Okay');
+        $matcher = $this->initURLMatcher($request);
+        $match = $matcher->match($request->getPathInfo());
+
+        if ($match) {
+            return new Response('Route found!!!');
+        }
+
+        return new Response('No route found');
+    }
+
+    private function initURLMatcher(Request $request)
+    {
+        $loader = new YamlFileLoader(new FileLocator(dirname(__DIR__, 2).'/config'));
+        $routes = $loader->load('routes.yaml');
+
+        $context = new RequestContext();
+        $context->fromRequest($request);
+
+        return new UrlMatcher($routes, $context);
     }
 }
